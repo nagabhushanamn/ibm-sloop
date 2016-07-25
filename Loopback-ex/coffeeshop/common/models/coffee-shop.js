2da6b95a9067125a9a1cbbbdc9106ceadd37ea12
nagabhushanamn <nagabhushanamn@gmail.com>
2016-07-25 13:20:16 +0530

    cofeeapp with acl

--- Loopback-ex/coffeeshop/common/models/coffee-shop.js
+++ Loopback-ex/coffeeshop/common/models/coffee-shop.js
@@ -1,34 +1,31 @@
-module.exports = function(Coffeeshop) {
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-getting-started-intermediate
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
 
-	Coffeeshop.status = function(cb) {
-		var currentDate = new Date();
-		var currentHour = currentDate.getHours();
-		var OPEN_HOUR = 6;
-		var CLOSE_HOUR = 20;
+module.exports = function(CoffeeShop) {
+  CoffeeShop.status = function(cb) {
+    var currentDate = new Date();
+    var currentHour = currentDate.getHours();
+    var OPEN_HOUR = 6;
+    var CLOSE_HOUR = 20;
 
-		console.log('Current hour is ' + currentHour);
+    console.log('Current hour is ' + currentHour);
 
-		var response;
-		if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
-			response = 'We are open for business.';
-		} else {
-			response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
-		}
+    var response;
+    if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
+      response = 'We are open for business.';
+    } else {
+      response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
+    }
+    cb(null, response);
+  };
 
-		cb(null, response);
-
-	};
-
-	// Add RemoteMethod
-	Coffeeshop.remoteMethod('status', {
-		http : {
-			path : '/status',
-			verb : 'get'
-		},
-		returns : {
-			arg : 'status',
-			type : 'string'
-		}
-	});
-
+  CoffeeShop.remoteMethod(
+    'status',
+    {
+      http: {path: '/status', verb: 'get'},
+      returns: {arg: 'status', type: 'string'}
+    }
+  );
 };
